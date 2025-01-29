10083a897549a8791e18e0530592bc99f9ff85ef
akash2003git <akash.tayade.dev@gmail.com>
2025-01-29 20:00:33 +0530

    Updates

---
+++ routes/reviews.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const Campground = require("../models/campground");
+const Review = require("../models/review");
+const { reviewSchema } = require("../schemas.js");
+const ExpressError = require("../utils/ExpressError");
+const catchAsync = require("../utils/catchAsync");
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+router.post(
+  "/",
+  validateReview,
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    // res.send(review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  }),
+);
+
+// The $pull operator removes from an existing array all instances of a value
+// or values that match a specified condition.
+router.delete(
+  "/:reviewId",
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+    // res.send("DELETED!");
+  }),
+);
+
+module.exports = router;
