00b7d90a134d4ee9499977e70cfa24da4684e41c
akash2003git <akash.tayade.dev@gmail.com>
2025-02-02 13:40:48 +0530

    Authorization

--- seeds/index.js
+++ seeds/index.js
@@ -19,6 +19,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: "679f04f05de7c9cd70980eb1",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: `https://picsum.photos/400?random=${Math.random()}`,
